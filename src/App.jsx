0f5d68a7f0ed006a7acd89063950a07f32ddc137
Metkiyd <sviridenko.dev@gmail.com>
2022-09-19 19:43:10 +0300

    Create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles/index.scss';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <p>
+          Edit and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
