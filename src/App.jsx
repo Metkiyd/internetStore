02f387f773009cc60ffbd661a3aeb31253fdd5b4
Metkiyd <sviridenko.dev@gmail.com>
2022-11-02 19:29:40 +0300

    products create

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import Product from './Components/Product/Product';
+import products from './data/products';
 import './styles/index.scss';
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Product product={products[0]} />
 		</div>
 	);
 }
---
+++ src/Components/Product/Product.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './product.scss'
+
+const Product = () => {
+
+	return (
+		<div className='container'>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+			<div className='product'>
+				Product
+			</div>
+		</div>
+	)
+}
+
+export default Product;
